7ea2af7134ddb8f15f55d1f412621c8cc2980e08
Ernane Sena <ernane.sena@gmail.com>
2017-11-13 21:29:04 -0300

    Commit Inicial

---
+++ active_file.gemspec
@@ -0,0 +1,10 @@
+require File.expand_path('../lib/active_file/version', __FILE__)
+
+Gem::Specification.new do |gem|
+  gem.name        = 'active_file'
+  gem.version     = ActiveFile::VERSION
+  gem.description = 'Just a file system database'
+  gem.summary     = 'Just a file system database'
+  gem.author      = 'Ernane Sena'
+  gem.files       = Dir['{lib/**/*.rb,lib/tasks/*.rake,README.md,Rakefile,active_file.gemspec}']
+end
---
+++ lib/active_file.rb
@@ -0,0 +1,92 @@
+require 'active_file/version'
+require 'yaml'
+
+module ActiveFile
+  def save
+    @new_record = false
+    File.open("db/revistas/#{@id}.yml", 'w') do |file|
+      file.puts serialize
+    end
+  end
+
+  def destroy
+    return unless @destroyed || @new_record
+    @destroyed = true
+    FileUtils.rm "db/revistas/#{@id}.yml"
+  end
+
+  module ClassMethods
+    def field(name, required: false, default: '')
+      @fields ||= []
+      @fields << Field.new(name, required, default)
+      class_eval %$
+          attr_accessor *#{@fields.map(&:name)}
+          attr_reader :id, :destroyed, :new_record
+          def initialize(#{@fields.map(&:to_argument).join(', ')})
+              @id = self.class.next_id
+              @destroyed = false
+              @new_record = true
+              #{@fields.map(&:to_assign).join("\n")}
+          end    $
+    end
+
+    def find(id)
+      unless File.exist?("db/revistas/#{id}.yml")
+        raise DocumentNotFound, "Arquivo #{id} não encontrado.", caller
+      end
+      YAML.load File.open("db/revistas/#{id}.yml", 'r')
+    end
+
+    def next_id
+      Dir.glob('db/revistas/*.yml').size + 1
+    end
+
+    def method_missing(name, *args, &block)
+      field = name.to_s.split('_').last
+      super if @fields.map(&:name).include? field
+      load_all.select do |object|
+        object.send(field) == args.first
+      end
+    end
+
+    private
+
+    def load_all
+      Dir.glob('db/revistas/*.yml').map do |file|
+        deserialize file
+      end
+    end
+
+    def deserialize(file)
+      YAML.load File.open(file, 'r')
+    end
+  end
+
+  class Field
+    attr_reader :name, :required, :default
+
+    def initialize(name, required, default)
+      @name     = name
+      @required = required
+      @default  = default
+    end
+
+    def to_argument
+      "#{@name}: #{@default}"
+    end
+
+    def to_assign
+      "@#{@name} = #{@name}"
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  private
+
+  def serialize
+    YAML.dump self
+  end
+end
